90a3d2d47227a5a237fcf044dbaa1faeffa04e5c
TriSem <tristan.semmler@gmx.de>
2019-04-28 21:23:55 +0200

    Extended EventSystem
    Events and EventListeners can now pass up to three parameters in their functions.

--- Assets/_Scripts/ScriptableObjects/Systems/GameEvent.cs
+++ Assets/_Scripts/ScriptableObjects/Systems/GameEvent.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Assets/Systems/GameEvent")]
+[CreateAssetMenu(fileName = "GameEvent", menuName = "Assets/Systems/GameEvent")]
+/** GameEvent without the option to pass data. */
 public class GameEvent : ScriptableObject
 {
     private List<GameEventListener> listeners = 
         new List<GameEventListener>();
-    
-    public void Raise()
+
+    public void NotifyListeners()
     {
         for(int i = listeners.Count - 1; i >= 0; i--)
         {
             listeners[i].OnEventRaised();
+        }
+    }
+
+    public void NotifyListeners<T>(T arg0)
+    {
+        for(int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised(arg0);
+        }
+    }
+
+    public void NotifyListeners<T0, T1>(T0 arg0, T1 arg1)
+    {
+        for(int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised(arg0, arg1);
+        }
+    }
+
+    public void NotifyListeners<T0, T1, T2>(T0 arg0, T1 arg1, T2 arg2)
+    {
+        for(int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised(arg0, arg1, arg2);
         }
     }
 
@@ -21,4 +46,9 @@ public class GameEvent : ScriptableObject
     
     public void UnregisterListener(GameEventListener listener)
         => listeners.Remove(listener);
+
+    public void RemoveAllListeners()
+    {
+        listeners.Clear();
+    }
 }
