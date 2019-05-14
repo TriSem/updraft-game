2fdb6af2632bf3d2fd5f5a5851ba2d8a8747c0dd
TriSem <tristan.semmler@gmx.de>
2019-05-14 11:46:40 +0200

    Added Score Text
    - There is an error in the event system preventing the score from being displayed.

--- Assets/_Scripts/MonoBehaviours/Objects/Pickup.cs
+++ Assets/_Scripts/MonoBehaviours/Objects/Pickup.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     [SerializeField] IntSO playerScore = null;
+    [SerializeField] GameEvent playerScoreChange = null;
 
     [SerializeField] int value = 0;
 
     void OnTriggerEnter()
     {
         playerScore.value += value;
+        playerScoreChange.NotifyListeners(playerScore.value);
         Destroy(this.gameObject);
     }
 }
---
+++ Assets/_Scripts/MonoBehaviours/Systems/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    TextMeshProUGUI text;
+
+    void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        text.text = "";
+    }
+
+    public void OnScoreChanged(int score)
+    {
+        text.text = score.ToString();
+    }
+}
