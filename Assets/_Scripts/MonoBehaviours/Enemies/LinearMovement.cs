01950c87a9b30730565bc8ff728f7a52dd14cc1c
TriSem <tristan.semmler@gmx.de>
2019-06-03 16:26:42 +0200

    Added HomingMovement.cs

--- Assets/_Scripts/MonoBehaviours/Enemies/LinearMovement.cs
+++ Assets/_Scripts/MonoBehaviours/Enemies/LinearMovement.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class LinearMovement : MonoBehaviour
 {
-    [SerializeField] Vector3 direction = Vector3.zero;
-    [SerializeField] float speed = 0f;
+    [SerializeField] protected Vector3 direction = Vector3.zero;
+    [SerializeField] protected float speed = 0f;
 
-    [SerializeField] float gizmoLength = 4f;
+    [SerializeField] protected float gizmoLength = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
         AlignToDirection();
+        direction.Normalize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += direction * speed * Time.deltaTime;
+        Move();
     }
 
     void OnDrawGizmos()
@@ -27,5 +28,9 @@ public class LinearMovement : MonoBehaviour
         Gizmos.DrawRay(transform.position, direction * gizmoLength);
     }
 
-    void AlignToDirection() => transform.right = direction;
+    protected void AlignToDirection() => transform.right = direction;
+
+    // Moves in a straight line.
+    protected void Move() => transform.position += direction * speed * Time.deltaTime;
+    
 }
