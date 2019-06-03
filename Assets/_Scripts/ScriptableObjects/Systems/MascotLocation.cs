b93319efaf2d88f6e3cd852cf8916c729fe8c744
TriSem <tristan.semmler@gmx.de>
2019-06-03 15:12:21 +0200

    Added MascotLocation.cs
    With this every Enemy can get readonly access to the mascots position.

---
+++ Assets/_Scripts/ScriptableObjects/Systems/MascotLocation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MascotLocation : ScriptableObject
+{
+    public Vector3 MascotPosition { get; private set; }
+
+    public void SetMascotPosition(Mascot mascot) => 
+        MascotPosition = mascot.transform.position;
+}
