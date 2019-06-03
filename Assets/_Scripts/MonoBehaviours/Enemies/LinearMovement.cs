32a6f98953f9cfa7601b61c321188a33a67e4cf8
TriSem <tristan.semmler@gmx.de>
2019-06-03 14:30:42 +0200

    Added LinearMovement.cs

---
+++ Assets/_Scripts/MonoBehaviours/Enemies/LinearMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearMovement : MonoBehaviour
+{
+    [SerializeField] Vector3 direction = Vector3.zero;
+    [SerializeField] float speed = 0f;
+
+    [SerializeField] float gizmoLength = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AlignToDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    void OnDrawGizmos()
+    {
+        AlignToDirection();
+        Gizmos.DrawRay(transform.position, direction * gizmoLength);
+    }
+
+    void AlignToDirection() => transform.right = direction;
+}
