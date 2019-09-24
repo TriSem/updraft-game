2f165f11e72796f8db3b4e78c0c6b8f35e6aeb59
TriSem <tristan.semmler@gmx.de>
2019-09-24 19:36:16 +0200

    Added Faux Projection
    Dynamically scales objects that move from back to front and vice versa to simulate perspective.

---
+++ Assets/_Scripts/MonoBehaviours/Systems/FauxProjection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FauxProjection : MonoBehaviour
+{
+    [SerializeField] float nearZ = 0f;
+    [SerializeField] float farZ = 100f;
+    [SerializeField] float maxScale = 1f;
+    [SerializeField] float minScale = 0.05f;
+
+
+    void Update()
+    {
+        float z = transform.position.z;
+        float scale = Mathf.Lerp(maxScale, minScale, z / farZ);
+        transform.localScale = Vector3.one * scale;
+    }
+
+    #if (UNITY_EDITOR)
+    void OnDrawGizmos()
+    {
+        float z = transform.position.z;
+        float scale = Mathf.Lerp(maxScale, minScale, z / farZ);
+        transform.localScale = Vector3.one * scale;
+    }
+    #endif
+}
