09e4bcdf92830285f9111c966a55cbb6e48c3632
TriSem <tristan.semmler@gmx.de>
2019-09-24 23:55:40 +0200

    Added RapidAttack.cs

---
+++ Assets/_Scripts/MonoBehaviours/Enemies/RapidAttack.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RapidAttack : MonoBehaviour
+{
+    [Tooltip("Amount of time before the attacker charges.")]
+    [SerializeField] float chargeTime = 3f;
+    [SerializeField] float speed = 20f;
+    [SerializeField] LineRenderer lineRenderer;
+
+    float timePassed = 0f;
+    Vector3 direction;
+
+    void Awake()
+    {
+        if(lineRenderer == null)
+            Debug.LogError("Forgot to set line renderer in editor.");
+        lineRenderer.SetPosition(0, transform.position);
+        direction = lineRenderer.GetPosition(1) - lineRenderer.GetPosition(0);
+        direction.Normalize();
+    }
+
+    void Update()
+    {
+        if(timePassed < chargeTime)
+        {
+            float lerpFactor = timePassed / chargeTime;
+            Color color = Color.Lerp(Color.cyan, Color.red, lerpFactor);
+            color.a = Mathf.Lerp(0, 1, lerpFactor);
+            lineRenderer.startColor = lineRenderer.endColor = color;
+            timePassed += Time.deltaTime;
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        transform.position += direction * Time.deltaTime * speed;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if(!Application.isPlaying)
+            lineRenderer.SetPosition(0, transform.position);
+    }
+}
